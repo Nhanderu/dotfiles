f751af8f2351786cc161730dc1f60389550e7eec
Rafael Escobar <rescobar@protonmail.com>
2017-03-05 15:35:16 -0300

    Add functions and packages.

--- pkgs.sh
+++ pkgs.sh
@@ -1,3 +1,19 @@
+yaourt -S --noconfirm \
+    aur/android-studio \
+    aur/dotnet-bin \
+    aur/popcorntime \
+    aur/spotify \
+    aur/stremio-bin \
+    aur/visual-studio-code \
+    community/chromium \
+    community/go \
+    community/mongodb \
+    community/rtorrent \
+    extra/chromium \
+    extra/mariadb \
+    extra/vlc \
+    multilib/steam
+
 # Install Visual Studio Code extensions.
 code --install-extension lukehoban.Go
 code --install-extension Poorchop.theme-darktooth
