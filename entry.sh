b32c7f57e2acb0b79236f61acf9abd92c197164a
Rafael Escobar <rescobar@protonmail.com>
2017-05-24 18:30:11 -0300

    Add functions to entry.

--- entry.sh
+++ entry.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 export DOTFILES=~/Projects/shell/dotfiles
-for file in {env,prompt}; do
+for file in {env,prompt,functions}; do
     [[ -f "$DOTFILES/$file.sh" ]] && source "$DOTFILES/$file.sh"
 done
