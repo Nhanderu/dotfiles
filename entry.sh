96740cfc9f418c4dca6c1effa2f71068c987c7d1
Rafael Escobar <rescobar@protonmail.com>
2017-07-02 11:06:51 -0300

    I'm rich now.

--- entry.sh
+++ entry.sh
@@ -3,3 +3,5 @@ export DOTFILES=~/Projects/shell/dotfiles
 for file in {env,prompt,functions}; do
     [[ -f "$DOTFILES/$file.sh" ]] && source "$DOTFILES/$file.sh"
 done
+
+[[ -d "$DOTFILES/macos" ]] && [[ -f "$DOTFILES/macos/entry.sh" ]] && source "$DOTFILES/macos/entry.sh"
