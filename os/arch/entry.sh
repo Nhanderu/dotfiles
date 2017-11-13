e53535841aefe36500d534bac0b7968b8580b133
Rafael Escobar <rescobar@protonmail.com>
2017-11-13 14:08:45 -0200

    Fix Arch specific scripts.

--- os/arch/entry.sh
+++ os/arch/entry.sh
@@ -1,5 +1,5 @@
 for file in {pkgs}; do
-    if [[ test -f "$DOTFILES/os/darwin/$file.sh" ]]; then
-        source "$DOTFILES/os/darwin/$file.sh"
+    if [[ test -f "$DOTFILES/os/arch/$file.sh" ]]; then
+        source "$DOTFILES/os/arch/$file.sh"
     fi
 done
