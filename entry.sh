6f7b06e7cab3fbb7fc34149c90a5351251e82b9e
Rafael Escobar <rescobar@protonmail.com>
2017-11-23 14:34:35 -0200

    Fix for loop lists and remove 'test'.

--- entry.sh
+++ entry.sh
@@ -3,29 +3,25 @@
 dotfiles=~/Projects/shell/dotfiles
 
 # Executes dotfiles entrypoint script.
-for file in {env,functions}; do
-    if [[ test -f "$dotfiles/$file.sh" ]]; then
+for file in {env,functions,}; do
+    if [[ -f "$dotfiles/$file.sh" ]]; then
         source "$dotfiles/$file.sh"
     fi
 done
 
 # Executes dotfiles' subpackages entrypoint scripts.
-for dir in {prompt}; do
-    if [[ test -f "$dotfiles/$dir/entry.sh" ]]; then
+for dir in {prompt,}; do
+    if [[ -f "$dotfiles/$dir/entry.sh" ]]; then
         source "$dotfiles/$dir/entry.sh"
     fi
 done
 
-current_os="$(uname | tr '[A-Z]' '[a-z]')"
-
 # Executes dotfiles' OS-specific entrypoint scripts,
 # if matches the current OS.
-if [[ test -d "$dotfiles/os" ]]; then
-    for entry in "$dotfiles/os/*/entry.sh"; do
-        if [[ test $entry = "$dotfiles/os/$current_os/entry.sh" ]]; then
-            source "$entry"
-        fi
-    done
+current_os="$(uname | tr '[A-Z]' '[a-z]')"
+current_os_entry="$dotfiles/os/$current_os/entry.sh"
+if [[ -f "$current_os_entry" ]]; then
+    source "$current_os_entry"
 fi
 
 export DOTFILES=$dotfiles
