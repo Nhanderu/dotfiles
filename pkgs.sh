2eb5fc11306a1ec5b10c55587e35f8709c3e158a
Rafael Escobar <rescobar@redventures.com>
2017-12-03 13:22:07 -0200

    All Code pkgs.

--- pkgs.sh
+++ pkgs.sh
@@ -1,3 +1,13 @@
 # Install Visual Studio Code extensions.
-code --install-extension lukehoban.Go
+code --install-extension wmaurer.change-case
 code --install-extension Poorchop.theme-darktooth
+code --install-extension PeterJausovec.vscode-docker
+code --install-extension waderyan.gitblame
+code --install-extension donjayamanne.githistory
+code --install-extension codezombiech.gitignore
+code --install-extension lukehoban.go
+code --install-extension sysoev.language-stylus
+code --install-extension mrmlnc.vscode-pugbeautify
+code --install-extension mrmlnc.vscode-puglint
+code --install-extension kalitaalexey.vscode-rust
+code --install-extension be5invis.toml
