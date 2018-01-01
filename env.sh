d45524623648087530cea7f370ff4acde07f2408
Rafael Escobar <rescobar@protonmail.com>
2018-01-01 02:06:16 -0200

    Alacritty as default terminal.

--- env.sh
+++ env.sh
@@ -5,6 +5,7 @@ export BROWSER=/usr/bin/chromium
 export EDITOR=/usr/bin/nano
 export PROJ="$HOME/Projects"
 export GOPATH="$PROJ/go"
+export TERMINAL="alacritty"
 
 export PATH+=":$GOPATH/bin"
 export PATH+=":$DOTFILES/cmd"
