c2e3b6e9d5b555907124313d717b53b5455ca32d
Rafael Escobar <rescobar@protonmail.com>
2018-01-02 16:05:27 -0200

    Configures xinit.

--- boot.sh
+++ boot.sh
@@ -8,23 +8,6 @@
 dotfiles="$HOME/projects/shell/dotfiles"
 source $dotfiles/env.sh
 
-# Configures monitors.
-monitor_left="HDMI-2"
-monitor_right="HDMI-1-1"
-
-xrandr \
-    --output $monitor_left \
-    --auto \
-    --primary
-
-xrandr \
-    --output $monitor_right \
-    --auto \
-    --right-of $monitor_left
-
-unset monitor_left
-unset monitor_right
-
 # Starts XOrg.
 if [ -z "$DISPLAY" ] && \
     [ -n "$XDG_VTNR" ] && \
--- env.sh
+++ env.sh
@@ -10,6 +10,8 @@ export XDG_CONFIG_HOME="$HOME/.config"
 export XDG_CACHE_HOME="$HOME/.cache"
 export XDG_DATA_HOME="$HOME/.local/share"
 
+export XINITRC="$XDG_CONFIG_HOME/X11/xinitrc"
+
 PATH="/usr/local/sbin"
 PATH+=":/usr/local/bin"
 PATH+=":/usr/bin"
