91ac2eab8c78178ff744ebe3135c329f6dfef248
Rafael Escobar <rescobar@protonmail.com>
2018-01-03 18:01:44 -0200

    Idk.

--- boot.sh
+++ boot.sh
@@ -9,9 +9,9 @@ dotfiles="$HOME/projects/shell/dotfiles"
 source $dotfiles/env.sh
 
 # Starts XOrg.
-if [ -z "$DISPLAY" ] && \
-    [ -n "$XDG_VTNR" ] && \
-    [ "$XDG_VTNR" -eq 1 ]; then
+if [[ -z "$DISPLAY" ]] && \
+    [[ -n "$XDG_VTNR" ]] && \
+    [[ "$XDG_VTNR" -eq 1 ]]; then
 
     startx
 fi
