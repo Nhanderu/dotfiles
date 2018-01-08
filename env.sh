661d98cfc7905cd77b1c42b0baa308fc12c1f7b8
Rafael Escobar <rescobar@protonmail.com>
2018-01-08 20:16:28 -0200

    Fix PATH and GOROOT.

--- env.sh
+++ env.sh
@@ -4,20 +4,26 @@ export TERMINAL=$(which alacritty)
 
 export PROJ="$HOME/projects"
 export DOTFILES="$PROJ/shell/dotfiles"
-export GOPATH="$PROJ/go"
 
 export XDG_CONFIG_HOME="$HOME/.config"
 export XDG_CACHE_HOME="$HOME/.cache"
 export XDG_DATA_HOME="$HOME/.local/share"
+
+export GOROOT="/usr/local/go"
+export GOPATH="$PROJ/go"
 
 export XINITRC="$XDG_CONFIG_HOME/X11/xinitrc"
 export CARGO_HOME="$XDG_DATA_HOME/cargo"
 export RUSTUP_HOME="$XDG_DATA_HOME/rustup"
 
 PATH="$DOTFILES/bin"
-PATH+=":/usr/local/sbin"
-PATH+=":/usr/local/bin"
+PATH+=":/bin"
+PATH+=":/sbin"
 PATH+=":/usr/bin"
+PATH+=":/usr/sbin"
+PATH+=":/usr/local/bin"
+PATH+=":/usr/local/sbin"
+PATH+=":$GOROOT/bin"
 PATH+=":$GOPATH/bin"
 PATH+=":$CARGO_HOME/bin"
 PATH+=":/usr/bin/site_perl"
