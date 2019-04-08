c7f8d416a926ba1462371b20732f909a858ebe8e
Rafael Escobar <rescobar@protonmail.com>
2019-04-08 14:32:39 -0300

    Várias baguncinhas para ajustar os serviços no macOS e instalar o chunkwm.

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,8 +5,8 @@ dotfiles="$HOME/projects/shell/dotfiles"
 source $dotfiles/env.sh
 
 # Copies configuration files.
-cp -r $DOTFILES/sh/$current_sh/. ~
-cp -r $DOTFILES/config/.         ~/.config
+cp -rp $DOTFILES/sh/$current_sh/. $HOME
+cp -rp $DOTFILES/config/.         $XDG_CONFIG_HOME
 
 # Make binaries executable.
 find $DOTFILES/bin -type f -exec chmod +x {} \;
