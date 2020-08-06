09d81f1e429bea75a1b5df58095a9f1eb09f1958
Rafael Escobar <rescobar@protonmail.com>
2020-08-06 17:00:41 -0300

    Add new Cargo env vars

--- env.sh
+++ env.sh
@@ -12,15 +12,18 @@ export XDG_CACHE_HOME="$HOME/.cache"
 export XDG_DATA_HOME="$HOME/.local/share"
 
 export XINITRC="$XDG_CONFIG_HOME/X11/xinitrc"
-export NVM_DIR="$XDG_CONFIG_HOME/nvm"
-export CARGO_HOME="$XDG_DATA_HOME/cargo"
-export RUSTUP_HOME="$XDG_DATA_HOME/rustup"
-export GOENV_ROOT="$XDG_DATA_HOME/goenv"
 
+export NVM_DIR="$XDG_CONFIG_HOME/nvm"
+
+export RUSTUP_HOME="$XDG_DATA_HOME/rustup"
+export CARGO_HOME="$XDG_DATA_HOME/cargo"
+export CARGO_NAME="Rafael Escobar"
+export CARGO_EMAIL="rescobar@protonmail.com"
+
+export GOENV_ROOT="$XDG_DATA_HOME/goenv"
+export GOENV_SHELL=bash
 export GOROOT="/usr/local/go"
 export GOPATH="$XDG_DATA_HOME/go"
-
-export GOENV_SHELL=bash
 
 PATH="$DOTFILES/bin"
 PATH+=":/bin"
