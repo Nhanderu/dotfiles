60b62d022ad234a658713878e0d29741963c5455
Rafael Escobar <rescobar@protonmail.com>
2020-08-09 14:34:43 -0300

    Use only one Fish startup file

--- config/fish/boot.fish
+++
@@ -1,7 +0,0 @@
-source "$__fish_config_dir/env.fish"
-
-if test -z "$DISPLAY"
-and test -n "$XDG_VTNR"
-and test "$XDG_VTNR" -eq 1
-    startx
-end
--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,1 +1,24 @@
-source "$__fish_config_dir/session.fish"
+source "$__fish_config_dir/env.fish"
+source "$__fish_config_dir/alias.fish"
+
+set -l current_os (uname | tr '[A-Z]' '[a-z]')
+
+find $DOTFILES/os/$current_os/bin -type f -exec chmod +x {} \;
+for file in $DOTFILES/os/$current_os/*.fish
+    if test -f "$file"
+        source "$file"
+    end
+end
+
+if test -z "$TMUX"
+    tmux attach
+    or tmux new-session
+    and exit
+end
+
+if status --is-login
+and test -z "$DISPLAY"
+and test -n "$XDG_VTNR"
+and test "$XDG_VTNR" -eq 1
+    startx
+end
