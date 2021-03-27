381cc4b6e807f2ee5311edfd699fb20dc627ada8
Rafael Escobar <rafael@esco.dev>
2021-03-27 11:54:05 -0300

    Move env to linux and remove MOZ_ENABLE_WAYLAND

--- config/fish/sh/env.sh
+++ config/fish/sh/env.sh
@@ -1,29 +1,13 @@
-export BROWSER=`which firefox`
-export EDITOR=`which nvim`
-export TERMINAL=`which alacritty`
-
-export PROJ="$HOME/projects"
-export DOTFILES="$PROJ/nhanderu/dotfiles"
+export BROWSER=$(which firefox)
+export EDITOR=$(which nvim)
+export TERMINAL=$(which alacritty)
 
 export XDG_CONFIG_HOME="$HOME/.config"
 export XDG_CACHE_HOME="$HOME/.cache"
 export XDG_DATA_HOME="$HOME/.local/share"
 
-export BEMENU_OPTS="\
-    --ignorecase \
-    --bottom \
-    --prompt=search \
-    --fn='FiraMono-Regular 10' \
-    --tb=#282828 \
-    --tf=#79740e \
-    --fb=#282828 \
-    --ff=#fdf4c1 \
-    --nb=#282828 \
-    --nf=#fdf4c1 \
-    --hb=#282828 \
-    --hf=#79740e \
-    --sb=#282828 \
-    --sf=#79740e"
+export PROJ="$HOME/projects"
+export DOTFILES="$PROJ/nhanderu/dotfiles"
 
 export RUSTUP_HOME="$XDG_DATA_HOME/rustup"
 export CARGO_HOME="$XDG_DATA_HOME/cargo"
--- os/linux/env.sh
+++ os/linux/env.sh
@@ -1,1 +1,21 @@
 export PATH+=":$DOTFILES/os/linux/bin"
+
+export XDG_CURRENT_DESKTOP=wayland
+export SDL_VIDEODRIVER=wayland
+
+export BEMENU_OPTS="\
+    --ignorecase \
+    --bottom \
+    --prompt=search \
+    --fn='FiraMono-Regular 10' \
+    --tb=#282828 \
+    --tf=#79740e \
+    --fb=#282828 \
+    --ff=#fdf4c1 \
+    --nb=#282828 \
+    --nf=#fdf4c1 \
+    --hb=#282828 \
+    --hf=#79740e \
+    --sb=#282828 \
+    --sf=#79740e \
+"
