88c6ca9321872d08be5e40f3967a0163bec8b761
Rafael Escobar <rafael@esco.dev>
2021-04-07 16:55:19 -0300

    Configure colors for Fish

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -3,6 +3,20 @@ bass source $__fish_config_dir/sh/os.sh
 
 alias tmux "tmux -f $XDG_CONFIG_HOME/tmux/tmux.conf"
 
+set fish_color_normal white
+set fish_color_command af3a03 # orange
+set fish_color_quote blue
+set fish_color_redirection fe8019 # brorange
+set fish_color_end yellow
+set fish_color_error red
+set fish_color_param fe8019 # brorange
+set fish_color_comment brblack
+set fish_color_operator yellow
+set fish_color_escape brblack
+set fish_color_autosuggestion brblack
+set fish_color_cancel red
+set fish_color_search_match green
+
 if status --is-login
 and test -z "$DISPLAY"
 and test (tty) = "/dev/tty1"
